27f1cc2252587afe948c76935741eaf4692948a0
mifukia <mifukiahelp@gmail.com>
2018-05-13 23:14:57 +0900

    nichiyoubi

--- models/sio.js
+++ models/sio.js
@@ -7,27 +7,28 @@ function sio(server) {
 
     //接続
     io.on('connection', (socket) => {
-        socket.on('login',(data) =>{
+        socket.on('login', (data) => {
             socket.broadcast.emit('login', {
-              username: data.name,
-              time : dateformat(new Date(), 'yyyy-mm-dd HH:MM:ss'),
+                username: data.name,
+                time: dateformat(new Date(), 'yyyy-mm-dd HH:MM:ss'),
             });
         });
-        socket.on('logout',(data) =>{
+        socket.on('logout', (data) => {
             socket.broadcast.emit('logout', {
-              username: data.name,
-              time : dateformat(new Date(), 'yyyy-mm-dd HH:MM:ss'),
+                username: data.name,
+                time: dateformat(new Date(), 'yyyy-mm-dd HH:MM:ss'),
             });
         })
         socket.on('chat message', (data) => {
             io.emit('chat message', {
-              username: data.name,
-              message: data.message,
-              time : dateformat(new Date(), 'yyyy-mm-dd HH:MM:ss'),
+                username: data.name,
+                message: data.message,
+                time: dateformat(new Date(), 'yyyy-mm-dd HH:MM:ss'),
             });
         });
         socket.on('inv', (data) => {
             console.log(data.address);
+
             sendmail({
                 from: 'simchat_for@yahoo.co.jp',
                 to: data.address,
